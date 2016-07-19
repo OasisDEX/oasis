722ac87751bbc2f60b4f6374b08d61e0b851fd04
Joris Bontje <jorisbontje@gmail.com>
2016-07-19 15:37:53 +0200

    remove debug log

--- frontend/client/templates/widgets/offermodal.js
+++ frontend/client/templates/widgets/offermodal.js
@@ -147,7 +147,6 @@ Template.offermodal.viewmodel({
   },
   buy: function () {
     var _id = this.templateInstance.data.offer._id
-    console.log('buy', this.templateInstance.data.offer.type(), this.total(), this.volume())
     if (this.templateInstance.data.offer.type() === 'bid') {
       Offers.buyOffer(_id, web3.toWei(new BigNumber(this.total())))
     } else {
