675d00cbb08bd5a98bd46b6c17f125e6591ef3dd
sunfeld <sunfeld@sunis.nl>
2016-08-29 12:07:43 +0200

    added button to disable orderbook limit. closes #22

--- frontend/imports/ui/client/helpers.js
+++ frontend/imports/ui/client/helpers.js
@@ -81,18 +81,41 @@ Template.registerHelper('lastTrades', () => {
     { buyWhichToken: quoteCurrency, sellWhichToken: baseCurrency },
     { buyWhichToken: baseCurrency, sellWhichToken: quoteCurrency },
   ] };
-  return Trades.find(obj, { sort: { blockNumber: -1, transactionIndex: -1 }, limit: 10 });
+  return Trades.find(obj, { sort: { blockNumber: -1, transactionIndex: -1 }, skip:1, limit: 10 });
+});
+
+Template.registerHelper('countOffers', (type) => {
+  const quoteCurrency = Session.get('quoteCurrency');
+  const baseCurrency = Session.get('baseCurrency');
+  
+  let options = {};
+  if (type === 'ask') {
+    options.sort = { ask_price: 1 };
+    return Offers.find({ buyWhichToken: quoteCurrency, sellWhichToken: baseCurrency}, options).count();
+  } else if (type === 'bid') {
+    options.sort = { ask_price: 1 };
+   return Offers.find({ buyWhichToken: baseCurrency, sellWhichToken: quoteCurrency}, options).count();
+  }else{
+   return 0;
+  }
 });
 
 Template.registerHelper('findOffers', (type) => {
   const quoteCurrency = Session.get('quoteCurrency');
   const baseCurrency = Session.get('baseCurrency');
+  const limit = Session.get('orderBookLimit');
+
+  let options = {};
+  if(limit){
+      options.limit = limit;
+  }
+  
   if (type === 'ask') {
-    return Offers.find({ buyWhichToken: quoteCurrency, sellWhichToken: baseCurrency },
-                       { sort: { ask_price: 1 }, limit: 10 });
+    options.sort = { ask_price: 1 };
+    return Offers.find({ buyWhichToken: quoteCurrency, sellWhichToken: baseCurrency }, options);
   } else if (type === 'bid') {
-    return Offers.find({ buyWhichToken: baseCurrency, sellWhichToken: quoteCurrency },
-                       { sort: { ask_price: 1 }, limit: 10 });
+    options.sort = { ask_price: 1 };
+    return Offers.find({ buyWhichToken: baseCurrency, sellWhichToken: quoteCurrency }, options);
   } else if (type === 'mine') {
     const or = [
       { buyWhichToken: quoteCurrency, sellWhichToken: baseCurrency },
--- frontend/imports/ui/client/widgets/orders.js
+++ frontend/imports/ui/client/widgets/orders.js
@@ -1,2 +1,23 @@
 import './orders.html';
 import './orderrow.js';
+import { $ } from 'meteor/jquery';
+
+Template.orders.helpers({
+    moreBtn: function() {
+        if(this.priceClass){
+            const orderBookLimit = Session.get('orderBookLimit');
+            const totalOrders = Blaze._globalHelpers.countOffers(this.priceClass);
+            return (totalOrders > this.orders.count() ? true : false);
+        }
+    },
+    orderCount: function(){
+        return parseInt(Blaze._globalHelpers.countOffers(this.priceClass));
+    }
+    
+});
+
+Template.orders.events({
+  'click .more': function(event, templateInstance) {
+        Session.set('orderBookLimit',0);
+    }
+});
