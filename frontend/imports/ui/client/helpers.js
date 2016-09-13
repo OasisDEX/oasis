1621fd20ffa1b5afe95d05b0f8268413a8fb21e3
Mariano Conti <nanexcool@gmail.com>
2016-09-13 13:39:36 -0300

    Remove console logs

--- frontend/imports/ui/client/helpers.js
+++ frontend/imports/ui/client/helpers.js
@@ -207,9 +207,7 @@ Template.registerHelper('formatPrice', (value, currency) => {
 
 Template.prettyError = function(error) {
   if (typeof error !== "string") {
-    console.log("Error was not a string... converting!");
     error = error.toString();
   }
-  console.log("Returning first line of error");
   return error.split('\n')[0];
 }
