61c738529d1285af3fc88d62a7780e8585e82655
Mariano Conti <nanexcool@gmail.com>
2016-12-26 16:40:43 -0300

    Charts dropdown now functional

--- frontend/imports/ui/client/widgets/chart.js
+++ frontend/imports/ui/client/widgets/chart.js
@@ -1,2 +1,14 @@
 import { Template } from 'meteor/templating';
 import './chart.html';
+
+Template.chart.viewmodel({
+  currentChart: 'DEPTH',
+  showDepth() {
+    return this.currentChart() === 'DEPTH' ? '' : 'hidden';
+  },
+  showVolume() {
+    return this.currentChart() === 'VOLUME' ? '' : 'hidden';
+  },
+  chartChange() {
+  },
+});
