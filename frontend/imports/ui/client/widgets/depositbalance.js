30f78dd5321c168cc624e591e89a101029088ab1
Mariano Conti <nanexcool@gmail.com>
2016-12-28 10:54:35 -0300

    Fix balance box in deposit

--- frontend/imports/ui/client/widgets/depositbalance.js
+++ frontend/imports/ui/client/widgets/depositbalance.js
@@ -1,2 +1,31 @@
+import { Session } from 'meteor/session';
 import { Template } from 'meteor/templating';
+
+import Tokens from '/imports/api/tokens';
+
 import './depositbalance.html';
+
+Template.depositbalance.viewmodel({
+  wethBalance() {
+    try {
+      const token = Tokens.findOne('W-ETH');
+      return web3.fromWei(token.balance).toString(10);
+    } catch (e) {
+      return '0';
+    }
+  },
+  wgntBalance() {
+    try {
+      const token = Tokens.findOne('W-GNT');
+      return web3.fromWei(token.balance).toString(10);
+    } catch (e) {
+      return '0';
+    }
+  },
+  selected(token) {
+    return token === this.baseCurrency() ? 'selected' : '';
+  },
+  baseChange() {
+    console.log('token');
+  },
+});
