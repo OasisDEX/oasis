3d09f6cc210b91e8187b5120e2719f771f5c6d47
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-01-09 17:35:00 -0300

    Allowing sell and buy orders to cross each other in the graph

--- frontend/imports/ui/client/widgets/chart.js
+++ frontend/imports/ui/client/widgets/chart.js
@@ -25,10 +25,10 @@ Template.chart.helpers({
   createDepthChart() {
     const quoteCurrency = Session.get('quoteCurrency');
     const baseCurrency = Session.get('baseCurrency');
-    const askIndexes = []; // Array of ask prices
-    const bidIndexes = []; // Array of bid prices
-    const askValues = []; // Array of ask amounts
-    let bidValues = []; // Array of bid amounts
+    const askPrices = []; // Array of ask prices
+    const bidPrices = []; // Array of bid prices
+    const askAmounts = []; // Array of ask amounts
+    let bidAmounts = []; // Array of bid amounts
 
     const bids = Offers.find({ buyWhichToken: baseCurrency, sellWhichToken: quoteCurrency },
                               { sort: { bid_price: 1 } }).fetch();
@@ -36,63 +36,95 @@ Template.chart.helpers({
                               { sort: { ask_price: 1 } }).fetch();
 
     asks.forEach(ask => {
-      const index = askIndexes.indexOf(ask.ask_price);
+      const index = askPrices.indexOf(ask.ask_price);
       if (index === -1) {
         // If it is the first order for this price
 
         // Keep track of new price index
-        askIndexes.push(ask.ask_price);
+        askPrices.push(ask.ask_price);
 
-        if (askValues.length > 0) {
+        if (askAmounts.length > 0) {
           // If there is a lower price we need to sum the amount of the previous price (to make a cumulative graph)
-          askValues.push(askValues[askValues.length - 1].add(new BigNumber(ask.sellHowMuch)));
+          askAmounts.push(askAmounts[askAmounts.length - 1].add(new BigNumber(ask.sellHowMuch)));
         } else {
-          askValues.push(new BigNumber(ask.sellHowMuch));
+          askAmounts.push(new BigNumber(ask.sellHowMuch));
         }
       } else {
         // If there was already another offer for the same price we add the new amount
-        askValues[index] = askValues[index].add(new BigNumber(ask.sellHowMuch));
+        askAmounts[index] = askAmounts[index].add(new BigNumber(ask.sellHowMuch));
       }
     });
 
     bids.forEach(bid => {
-      const index = bidIndexes.indexOf(bid.bid_price);
+      const index = bidPrices.indexOf(bid.bid_price);
       if (index === -1) {
         // If it is the first order for this price
 
         // Keep track of new price index and value
-        bidIndexes.push(bid.bid_price);
-        bidValues.push(new BigNumber(bid.buyHowMuch));
+        bidPrices.push(bid.bid_price);
+        bidAmounts.push(new BigNumber(bid.buyHowMuch));
       } else {
-        bidValues[index] = bidValues[index].add(new BigNumber(bid.buyHowMuch));
+        bidAmounts[index] = bidAmounts[index].add(new BigNumber(bid.buyHowMuch));
       }
 
       // It is necessary to update all the previous prices adding the actual amount (to make a cumulative graph)
-      bidValues = $.map(bidValues, (b, i) => ((i < bidValues.length - 1) ? b.add(bid.buyHowMuch) : b));
+      bidAmounts = $.map(bidAmounts, (b, i) => ((i < bidAmounts.length - 1) ? b.add(bid.buyHowMuch) : b));
     });
+
+    // All price values (bids & asks)
+    const vals = bidPrices.concat(askPrices).sort((a, b) => (a < b ? -1 : 1));
+
+    // Preparing arrays for graph
+    const askAmountsGraph = [];
+    const bidAmountsGraph = [];
+    let index = null;
+    let amount = null;
+
+    for (let i = 0; i < vals.length; i++) {
+      index = askPrices.indexOf(vals[i]);
+      if (index !== -1) {
+        // If there is a specific value for the price in asks, we add it
+        amount = EthTools.formatBalance(askAmounts[index].toNumber()).replace(',', '');
+      } else if (vals[i] < askPrices[0] || vals[i] > askPrices[askPrices.length - 1]) {
+        // If the price is lower or higher than the asks range there is not value to print in the graph
+        amount = {}; // Fake NULL as the library doesn't allow it
+      } else {
+        // If there is not an ask amount for this price, we need to add the previous amount
+        amount = askAmountsGraph[askAmountsGraph.length - 1];
+      }
+      askAmountsGraph.push({ x: vals[i], y: amount });
+
+      index = bidPrices.indexOf(vals[i]);
+      if (index !== -1) {
+        // If there is a specific value for the price in bids, we add it
+        amount = EthTools.formatBalance(bidAmounts[index].toNumber()).replace(',', '');
+      } else if (vals[i] < bidPrices[0] || vals[i] > bidPrices[bidPrices.length - 1]) {
+         // If the price is lower or higher than the bids range there is not value to print in the graph
+        amount = {}; // Fake NULL as the library doesn't allow it
+      } else {
+        // If there is not a bid amount for this price, we need to add the next available amount
+        for (let j = 0; j < askPrices.length; j++) {
+          if (bidPrices[j] >= vals[i]) {
+            amount = EthTools.formatBalance(bidAmounts[j].toNumber()).replace(',', '');
+            break;
+          }
+        }
+      }
+      bidAmountsGraph.push({ x: vals[i], y: amount });
+    }
 
     // Use Meteor.defer() to craete chart after DOM is ready:
     Meteor.defer(() => {
       const ctx = document.getElementById('market-chart-depth');
 
-
-      const bidValuesGraph = $.map(bidValues,
-        (b, i) => ({ x: bidIndexes[i], y: EthTools.formatBalance(b.toNumber()).replace(',', '') }));
-
-      // It is necessary to append the bid values to get the necessary offset for ask values
-      const askValuesGraph = $.map(bidValues, () => askValues[askValues.length - 1]).concat(
-        $.map(askValues,
-        (a, i) => ({ x: askIndexes[i], y: EthTools.formatBalance(a.toNumber()).replace(',', '') })));
-
-
       const myChart = new Chart(ctx, {
         type: 'line',
         data: {
-          labels: $.map(bidIndexes.concat(askIndexes), (val) => val.toFixed(5)),
+          labels: $.map(vals, (v) => v.toFixed(5).replace(/0{0,3}$/, '')),
           datasets: [
             {
               label: 'Buy',
-              data: bidValuesGraph,
+              data: bidAmountsGraph,
               backgroundColor: 'rgba(38, 166, 154, 0.2)',
               borderColor: 'rgba(38, 166, 154, 1)',
               borderWidth: 2,
@@ -101,7 +133,7 @@ Template.chart.helpers({
             },
             {
               label: 'Sell',
-              data: askValuesGraph,
+              data: askAmountsGraph,
               backgroundColor: 'rgba(239, 83, 80, 0.2)',
               borderColor: 'rgba(239, 83, 80, 1)',
               borderWidth: 2,
