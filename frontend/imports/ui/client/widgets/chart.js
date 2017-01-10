36e2a54a5642ac67f8ecd2e0a40791677a0da7eb
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-01-10 15:50:54 -0300

    Optimizing volume chart

--- frontend/imports/ui/client/widgets/chart.js
+++ frontend/imports/ui/client/widgets/chart.js
@@ -184,33 +184,24 @@ Template.chart.viewmodel({
       const quoteCurrency = Session.get('quoteCurrency');
       const baseCurrency = Session.get('baseCurrency');
       const days = [];
-      const vol = [];
+      const vol = {};
       let day = null;
 
       for (let i = 6; i >= 0; i--) {
-        days.push(moment(Date.now()).startOf('day').subtract(i, 'days'));
-        vol.push(new BigNumber(0));
+        day = moment(Date.now()).startOf('day').subtract(i, 'days');
+        days.push(day);
+        vol[day.unix()] = new BigNumber(0);
       }
 
       const trades = Trades.find({ $or: [
         { buyWhichToken: baseCurrency, sellWhichToken: quoteCurrency },
         { buyWhichToken: quoteCurrency, sellWhichToken: baseCurrency },
       ],
-        timestamp: { $gte: days[0].unix() },
+        timestamp: { $gte: days[0].unix() / 1000 },
       });
 
       trades.forEach((trade) => {
-        day = 0;
-
-        for (let i = 1; i <= 7; i++) {
-          if (i === 7) {
-            day = 6;
-            break;
-          } else if (trade.timestamp < days[i].unix()) {
-            day = i - 1;
-            break;
-          }
-        }
+        day = moment(trade.timestamp * 1000).startOf('day').unix();
 
         if (trade.buyWhichToken === quoteCurrency) {
           vol[day] = vol[day].add(new BigNumber(trade.sellHowMuch));
@@ -218,11 +209,13 @@ Template.chart.viewmodel({
           vol[day] = vol[day].add(new BigNumber(trade.buyHowMuch));
         }
       });
+
       const myChart = this.volumeChart();
-      myChart.data.labels = $.map(days, (d) => d.format('ll'));
+      myChart.data.labels = days.map((d) => d.format('ll'));
+
       myChart.data.datasets = [{
         label: 'Volume',
-        data: $.map(vol, (v) => EthTools.formatBalance(v.toNumber()).replace(',', '')),
+        data: Object.keys(vol).map((key) => EthTools.formatBalance(vol[key].toNumber()).replace(',', '')),
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 2,
