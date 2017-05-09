606d92f9e2800cd8410494746aae0f58d7e2258d
Mariano Conti <nanexcool@gmail.com>
2017-05-09 12:49:08 -0300

    Check 3 times for web3 before going localhost

--- frontend/packages/dapple/package-pre-init.js
+++ frontend/packages/dapple/package-pre-init.js
@@ -3,18 +3,33 @@ import { Session } from 'meteor/session';
 
 web3Obj = new Web3();
 Session.set('web3ObjReady', false);
-setTimeout(
+Session.set('web3Counter', 0);
+
+const web3Interval = setInterval(
   function() {
     if (window.web3) {
       web3Obj.setProvider(window.web3.currentProvider);
+      window.web3 = web3Obj;
+      Session.set('web3ObjReady', true);
       console.log('Using current provider');
+      clearInterval(Session.get('web3Interval'));
+      Session.delete('web3Interval');
     } else {
-      web3Obj.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
-      console.log('Using new provider');
+      let counter = Session.get('web3Counter');
+      counter++;
+      Session.set('web3Counter', counter);
+      if (counter >= 3) {
+        clearInterval(Session.get('web3Interval'));
+        Session.delete('web3Interval');
+        web3Obj.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+        console.log('Using new provider');
+        window.web3 = web3Obj;
+        Session.set('web3ObjReady', true);
+      }
     }
-    window.web3 = web3Obj;
-    Session.set('web3ObjReady', true);
   }, 300)
+
+Session.set('web3Interval', web3Interval);
 // console.log('package-pre-init done')
 
 if (typeof module !== 'undefined' && module.exports) {
