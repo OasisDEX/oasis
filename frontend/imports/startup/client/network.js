7c6dbebd571a9f8e89e5777436f9387c31cd77e7
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-05-09 09:50:47 -0300

    test web3

--- frontend/imports/startup/client/network.js
+++ frontend/imports/startup/client/network.js
@@ -22,7 +22,7 @@ function checkAccounts() {
           web3.eth.defaultAccount = localStorage.getItem('address');
         } else if (_.contains(accounts, Session.get('address'))) {
           web3.eth.defaultAccount = Session.get('address');
-        } else if (accounts.length > 0) {
+        } else if (accounts && accounts.length > 0) {
           web3.eth.defaultAccount = accounts[0];
         } else {
           web3.eth.defaultAccount = undefined;
@@ -55,66 +55,68 @@ function checkMarketOpen() {
 
 // CHECK FOR NETWORK
 function checkNetwork() {
-  web3.version.getNode((error) => {
-    const isConnected = !error;
+  if (Session.get('web3Ready') && typeof web3 !== 'undefined') {
+    web3.version.getNode((error) => {
+      const isConnected = !error;
 
-    // Check if we are synced
-    if (isConnected) {
-      web3.eth.getBlock('latest', (e, res) => {
-        if (!e) {
-          if (res && res.number >= Session.get('latestBlock')) {
-            Session.set('outOfSync', e != null || (new Date().getTime() / 1000) - res.timestamp > 600);
-            Session.set('latestBlock', res.number);
-            if (Session.get('startBlock') === 0) {
-              console.log(`Setting startblock to ${res.number - 6000}`);
-              Session.set('startBlock', (res.number - 6000));
+      // Check if we are synced
+      if (isConnected) {
+        web3.eth.getBlock('latest', (e, res) => {
+          if (!e) {
+            if (res && res.number >= Session.get('latestBlock')) {
+              Session.set('outOfSync', e != null || (new Date().getTime() / 1000) - res.timestamp > 600);
+              Session.set('latestBlock', res.number);
+              if (Session.get('startBlock') === 0) {
+                console.log(`Setting startblock to ${res.number - 6000}`);
+                Session.set('startBlock', (res.number - 6000));
+              }
+            } else {
+              // XXX MetaMask frequently returns old blocks
+              // https://github.com/MetaMask/metamask-plugin/issues/504
+              console.debug('Skipping old block');
             }
           } else {
-            // XXX MetaMask frequently returns old blocks
-            // https://github.com/MetaMask/metamask-plugin/issues/504
-            console.debug('Skipping old block');
-          }
-        } else {
-          console.debug('There is error while getting the latest block! ', e);
-        }
-      });
-    }
-
-    // Check which network are we connected to
-    // https://github.com/ethereum/meteor-dapp-wallet/blob/90ad8148d042ef7c28610115e97acfa6449442e3/app/client/lib/ethereum/walletInterface.js#L32-L46
-    if (!Session.equals('isConnected', isConnected)) {
-      if (isConnected === true) {
-        web3.eth.getBlock(0, (e, res) => {
-          let network = false;
-          if (!e) {
-            switch (res.hash) {
-              case '0xa3c565fc15c7478862d50ccd6561e3c06b24cc509bf388941c25ea985ce32cb9':
-                network = 'kovan';
-                Session.set('AVGBlocksPerDay', 21600);
-                break;
-              case '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d':
-                network = 'ropsten';
-                Session.set('AVGBlocksPerDay', 5760);
-                break;
-              case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
-                network = 'main';
-                Session.set('AVGBlocksPerDay', 5760);
-                break;
-              default:
-                network = 'private';
-            }
-          }
-          if (!Session.equals('network', network)) {
-            initNetwork(network, isConnected);
+            console.debug('There is error while getting the latest block! ', e);
           }
         });
-      } else {
-        Session.set('isConnected', isConnected);
-        Session.set('network', false);
-        Session.set('latestBlock', 0);
       }
-    }
-  });
+
+      // Check which network are we connected to
+      // https://github.com/ethereum/meteor-dapp-wallet/blob/90ad8148d042ef7c28610115e97acfa6449442e3/app/client/lib/ethereum/walletInterface.js#L32-L46
+      if (!Session.equals('isConnected', isConnected)) {
+        if (isConnected === true) {
+          web3.eth.getBlock(0, (e, res) => {
+            let network = false;
+            if (!e) {
+              switch (res.hash) {
+                case '0xa3c565fc15c7478862d50ccd6561e3c06b24cc509bf388941c25ea985ce32cb9':
+                  network = 'kovan';
+                  Session.set('AVGBlocksPerDay', 21600);
+                  break;
+                case '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d':
+                  network = 'ropsten';
+                  Session.set('AVGBlocksPerDay', 5760);
+                  break;
+                case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
+                  network = 'main';
+                  Session.set('AVGBlocksPerDay', 5760);
+                  break;
+                default:
+                  network = 'private';
+              }
+            }
+            if (!Session.equals('network', network)) {
+              initNetwork(network, isConnected);
+            }
+          });
+        } else {
+          Session.set('isConnected', isConnected);
+          Session.set('network', false);
+          Session.set('latestBlock', 0);
+        }
+      }
+    });
+  }
 }
 
 $(window).on('hashchange', () => {
@@ -162,36 +164,38 @@ Meteor.startup(() => {
   initSession();
   checkNetwork();
 
-  web3.eth.filter('latest', () => {
-    Tokens.sync();
-    Transactions.sync();
-    TokenEvents.syncTimestamps();
-  });
+  if (Session.get('web3Ready')) {
+    web3.eth.filter('latest', () => {
+      Tokens.sync();
+      Transactions.sync();
+      TokenEvents.syncTimestamps();
+    });
 
-  web3.eth.isSyncing((error, sync) => {
-    if (!error) {
-      Session.set('syncing', sync !== false);
+    web3.eth.isSyncing((error, sync) => {
+      if (!error) {
+        Session.set('syncing', sync !== false);
 
-      // Stop all app activity
-      if (sync === true) {
-        // We use `true`, so it stops all filters, but not the web3.eth.syncing polling
-        web3.reset(true);
-        checkNetwork();
-      // show sync info
-      } else if (sync) {
-        Session.set('startingBlock', sync.startingBlock);
-        Session.set('currentBlock', sync.currentBlock);
-        Session.set('highestBlock', sync.highestBlock);
-      } else {
-        Session.set('outOfSync', false);
-        Offers.sync();
-        web3.eth.filter('latest', () => {
-          Tokens.sync();
-          Transactions.sync();
-        });
+        // Stop all app activity
+        if (sync === true) {
+          // We use `true`, so it stops all filters, but not the web3.eth.syncing polling
+          web3.reset(true);
+          checkNetwork();
+        // show sync info
+        } else if (sync) {
+          Session.set('startingBlock', sync.startingBlock);
+          Session.set('currentBlock', sync.currentBlock);
+          Session.set('highestBlock', sync.highestBlock);
+        } else {
+          Session.set('outOfSync', false);
+          Offers.sync();
+          web3.eth.filter('latest', () => {
+            Tokens.sync();
+            Transactions.sync();
+          });
+        }
       }
-    }
-  });
+    });
+  }
 
   function syncAndSetMessageOnError(document) {
     Offers.syncOffer(document.object.id);
--- frontend/packages/dapple/package-pre-init.js
+++ frontend/packages/dapple/package-pre-init.js
@@ -1,12 +1,14 @@
 // console.log('package-pre-init start')
-if (typeof web3 !== 'undefined') {
-  console.log('Reusing web3.currentProvider');
-  web3 = new Web3(web3.currentProvider);
-} else if (typeof window.web3 !== 'undefined') {
-  console.log('Reusing window.web3.currentProvider');
-  web3 = new Web3(window.web3.currentProvider);
-} else {
-  console.log('New Web3.providers.HttpProvider');
-  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-}
+web3 = new Web3();
+
+setTimeout(
+  function() {
+    if (window.web3) {
+      web3.setProvider(window.web3.currentProvider);
+    } else {
+      web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+    }
+    window.web3 = web3;
+    Session.set('web3Ready', true);
+  }, 500)
 // console.log('package-pre-init done')
