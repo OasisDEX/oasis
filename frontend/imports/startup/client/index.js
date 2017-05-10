7c295a559c0d8141ff3e94dd1f2441d8db4d908e
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-05-10 17:01:58 -0300

    Adding metamask account locked message + replacing Maker Market to OasisDEX in landings

--- frontend/imports/startup/client/index.js
+++ frontend/imports/startup/client/index.js
@@ -32,7 +32,7 @@ import '../../ui/client/headers/volumes.html';
 import '../../ui/client/index.html';
 import '../../ui/client/footer.html';
 import '../../ui/client/noethereum.html';
-import '../../ui/client/noaccount.html';
+import '../../ui/client/noaccount.js';
 import '../../ui/client/whatisthis.html';
 
 import '../../ui/client/helpers.js';
---
+++ frontend/imports/ui/client/noaccount.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+import { web3Obj } from 'meteor/makerotc:dapple';
+
+import './noaccount.html';
+
+Template.noAccount.helpers({
+  metamask: function metamask() {
+    return web3Obj &&
+            web3Obj.currentProvider &&
+            web3Obj.currentProvider.constructor.name === 'MetamaskInpageProvider';
+  },
+});
