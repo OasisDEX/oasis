4df440e751e6ac61bab5760a0800d84a814fa444
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-05-10 19:26:08 +0200

    Closed transactions are clickable and redirect to etherscan transaction details

--- frontend/imports/ui/client/widgets/mytrades.js
+++ frontend/imports/ui/client/widgets/mytrades.js
@@ -4,6 +4,7 @@ import { Status } from '/imports/api/offers';
 import './mytrades.html';
 
 Template.mytrades.viewmodel({
+  status: Status,
   orderStatus: [Status.OPEN, Status.CLOSED],
   filterByStatus: Status.OPEN,
 });
