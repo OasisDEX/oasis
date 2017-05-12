6b3fa1e04ac66d37fb6a16b013d3e5231b4b72da
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-05-12 10:00:39 -0300

    Not considering dust orders for best price order alert

--- frontend/imports/ui/client/widgets/offermodal.js
+++ frontend/imports/ui/client/widgets/offermodal.js
@@ -225,10 +225,14 @@ Template.offermodal.viewmodel({
   },
   alertBetterOffer() {
     let bestOffer = null;
+    const dustLimitMap = Session.get('orderBookDustLimit');
+    const dustLimit = dustLimitMap[Session.get('quoteCurrency')] ? dustLimitMap[Session.get('quoteCurrency')] : 0;
+
     if (this.type() === 'bid') {
       bestOffer = Offers.findOne({
         buyWhichToken: Session.get('baseCurrency'),
         sellWhichToken: Session.get('quoteCurrency'),
+        sellHowMuch_filter: { $gte: dustLimit },
       },
         {
           sort: { ask_price_sort: 1 },
@@ -238,6 +242,7 @@ Template.offermodal.viewmodel({
       bestOffer = Offers.findOne({
         buyWhichToken: Session.get('quoteCurrency'),
         sellWhichToken: Session.get('baseCurrency'),
+        buyHowMuch_filter: { $gte: dustLimit },
       },
         {
           sort: { ask_price_sort: 1 },
