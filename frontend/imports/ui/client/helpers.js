179e899a3284cf49b9c77d13262438feb806af25
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-06-05 08:01:52 +0200

    Better offer suggestion is visible only if buy() is enabled on the market

--- frontend/imports/ui/client/helpers.js
+++ frontend/imports/ui/client/helpers.js
@@ -422,4 +422,5 @@ Template.registerHelper('volumeSelector', () => Session.get('volumeSelector'));
 
 Template.registerHelper('isMatchingEnabled', () => Session.get('isMatchingEnabled'));
 
-Template.registerHelper('isBuyEnabled', () => Session.get('isBuyEnabled'));
+Template.registerHelper('isBuyEnabled', () => !Session.get('idMatchingEnabled') ||
+                                              (Session.get('isBuyEnabled') && Session.get('isMatchingEnabled')));
