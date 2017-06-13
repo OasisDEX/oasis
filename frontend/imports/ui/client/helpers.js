a83de136a48d46c0d1e7f389b30cfe235357f32c
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-06-13 11:24:53 +0200

    Fix typo

--- frontend/imports/ui/client/helpers.js
+++ frontend/imports/ui/client/helpers.js
@@ -469,5 +469,5 @@ Template.registerHelper('volumeSelector', () => Session.get('volumeSelector'));
 
 Template.registerHelper('isMatchingEnabled', () => Session.get('isMatchingEnabled'));
 
-Template.registerHelper('isBuyEnabled', () => !Session.get('idMatchingEnabled') ||
+Template.registerHelper('isBuyEnabled', () => !Session.get('isMatchingEnabled') ||
 (Session.get('isBuyEnabled') && Session.get('isMatchingEnabled')));
