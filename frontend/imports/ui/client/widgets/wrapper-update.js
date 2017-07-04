95362b64d11c41dc8b09ad37172246aaa0c95628
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-07-04 10:16:50 +0200

    Progress bar showing the unwrapping process. Modal is closed automatically after the unwrapping is done.

--- frontend/imports/ui/client/widgets/wrapper-update.js
+++ frontend/imports/ui/client/widgets/wrapper-update.js
@@ -4,6 +4,7 @@ import { web3Obj } from 'meteor/makerotc:dapple';
 
 import Transactions from '/imports/api/transactions';
 import { formatError } from '/imports/utils/functions';
+import './progress-bar.js';
 import './wrapper-update.html';
 
 const TRANSACTION_TYPE_WITHDRAW = 'ethtokens_withdraw';
@@ -11,28 +12,50 @@ const WITHDRAW_GAS = 150000;
 const WITHDRAW = 'withdraw';
 
 Template.wrapperUpdate.viewmodel({
+  message: '',
+  current: 0,
+  inProgress: false,
+
+  resetProgressBar() {
+    this.current(0);
+    this.message('');
+    this.inProgress(false);
+  },
+  onInterruptedWrapping(error) {
+    this.resetProgressBar();
+    this.message('Unwrapping interrupted! Please try again!');
+    console.debug('Received error during unwrapping: ', error);
+  },
   unwrap() {
     const amount = Session.get('oldWrapperBalance');
+    this.inProgress(true);
     Dapple.getToken('OW-ETH', (error, token) => {
       if (!error) {
-        Session.set('ETHWithdrawProgress', 33);
-        Session.set('ETHWithdrawProgressMessage', 'Starting unwrap... (waiting for your approval)');
-        Session.set('ETHWithdrawErrorMessage', '');
+        this.current(33);
+        this.message('Start unwrapping ...');
         token.withdraw(amount.toString(10), { gas: WITHDRAW_GAS }, (txError, tx) => {
           if (!txError) {
-            Session.set('ETHWithdrawProgress', 66);
-            Session.set('ETHWithdrawProgressMessage', 'Executing unwrap... (waiting for transaction confirmation)');
+            this.current(66);
+            this.message('Unwrapping... (waiting for transaction confirmation)');
             Transactions.add(TRANSACTION_TYPE_WITHDRAW, tx, { type: WITHDRAW, amount: amount.toString(10) });
+            Transactions.observeRemoved(TRANSACTION_TYPE_WITHDRAW, (document) => {
+              if (document.receipt.logs.length === 0) {
+                this.onInterruptedWrapping('Missing logs inside the receipt!');
+              } else {
+                this.current(100);
+                this.message('Unwrapping Done!');
+                setTimeout(() => {
+                  this.inProgress(false);
+                  $('#wrapperUpdate').modal('hide');
+                }, 1000);
+              }
+            });
           } else {
-            Session.set('ETHWithdrawProgress', 0);
-            Session.set('ETHWithdrawProgressMessage', '');
-            Session.set('ETHWithdrawErrorMessage', formatError(txError));
+            this.onInterruptedWrapping(txError);
           }
         });
       } else {
-        Session.set('ETHWithdrawProgress', 0);
-        Session.set('ETHWithdrawProgressMessage', '');
-        Session.set('ETHWithdrawErrorMessage', error.toString());
+        this.onInterruptedWrapping(error);
       }
     });
   },
