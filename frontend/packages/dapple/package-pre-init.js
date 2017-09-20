e7cdb82dc61952f0eb8fcb4ec1884d92a711ca74
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-09-20 14:29:47 +0200

    Use mascara from script

--- frontend/packages/dapple/package-pre-init.js
+++ frontend/packages/dapple/package-pre-init.js
@@ -1,11 +1,11 @@
 // console.log('package-pre-init start')
 import { Session } from 'meteor/session';
 
-const metamask = require('metamascara');
+// const metamask = require('metamascara');
 
 Session.set('web3ObjReady', false);
 
-web3Obj = new Web3(metamask.createDefaultProvider());
+web3Obj = new Web3(metamask.createDefaultProvider({}));
 initWeb3();
 
 function initWeb3() {
